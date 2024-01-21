98c57d2eef40fdf05cbf9afd142d7d57c4063793
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2024-01-21 20:22:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@LyricsC/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  printf("Oh, well imagine /\n");
+  printf("As I'm pacing the pews in a church corridor /\n");
+  printf("And I can't help but to hear /\n");
+  printf("No, I can't help but to hear an exchanging of words...\n\n");
+
+  printf("Oh-oh-oh-oh-oh,\n");
+  printf("oh-oh-oh-oh,\n");
+  printf("oh-oh-oh /\n");
+  printf("Caught in a bad romance...\n\n");
+
+  printf("Coming out of my cage\n");
+  printf("And I've been doing just fine\n");
+  printf("Gotta gotta be down\n");
+  printf("Because I want it all\n\n");
+
+  printf("Now, this is a story all about how\n");
+  printf("My life got flipped-turned upside down\n");
+  printf("And I'd like to take a minute\n");
+  printf("Just sit right there\n");
+  printf("I'll tell you how I became the prince of a town called Bel-Air\n");
+  
+  return 0;
+}
